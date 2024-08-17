5747bb41405b3177b163f88ee6041f46ba135741
Marius Storhaug <marstor@hotmail.com>
2024-08-17 19:21:17 +0200

    Fix

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,18 +1,14 @@
 [CmdletBinding()]
 param()
-try {
-    $params = @{
-        Name            = 'GitHub'
-        Repository      = 'PSGallery'
-        TrustRepository = $true
-        Prerelease      = $env:GITHUB_ACTION_INPUT_Prerelease -eq 'true'
-    }
-    if (-not [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_Version)) {
-        $params['Version'] = $env:GITHUB_ACTION_INPUT_Version
-    }
-    Install-PSResource @params
-    Import-Module -Name 'GitHub' -Force
-} catch {
-    Write-Error $_
-    exit 1
+
+$params = @{
+    Name            = 'GitHub'
+    Repository      = 'PSGallery'
+    TrustRepository = $true
+    Prerelease      = $env:GITHUB_ACTION_INPUT_Prerelease -eq 'true'
 }
+if (-not [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_Version)) {
+    $params['Version'] = $env:GITHUB_ACTION_INPUT_Version
+}
+Install-PSResource @params
+Import-Module -Name 'GitHub' -Force
