403726fcea877321a6142e7e6c5fda2eb2027435
Marius Storhaug <marstor@hotmail.com>
2024-08-17 18:34:48 +0200

    test

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,13 +1,10 @@
 [CmdletBinding()]
 param()
 
-Write-Host '::group::Install GitHub'
 $params = @{
     Name            = 'GitHub'
     Repository      = 'PSGallery'
     TrustRepository = $true
-    Verbose         = $true
 }
 Install-PSResource @params
-Import-Module -Name 'GitHub' -Force -Verbose
-Write-Host '::endgroup::'
+Import-Module -Name 'GitHub' -Force
