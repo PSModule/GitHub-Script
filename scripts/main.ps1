03e5bfec288e12f03e64ae79cc9bb239b8505b4d
Marius Storhaug <marstor@hotmail.com>
2024-08-18 14:38:01 +0200

    Test

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -5,7 +5,9 @@ $Name = 'GitHub'
 $Version = [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_Version) ? $null : $env:GITHUB_ACTION_INPUT_Version
 $Prerelease = $env:GITHUB_ACTION_INPUT_Prerelease -eq 'true'
 
-$installedModules = Get-InstalledPSResource
+$installedModules = Get-InstalledPSResource -ErrorAction SilentlyContinue
+Write-Verbose "Installed modules:"
+Write-Verbose ($installedModules | Out-String)
 $alreadyInstalled = $installedModules | Where-Object Name -EQ $Name
 if ($Version) {
     $alreadyInstalled = $alreadyInstalled | Where-Object Version -EQ $Version
