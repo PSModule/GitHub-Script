6b1a7079f28697e0013d2d24b0827a12a40fa532
Marius Storhaug <marstor@hotmail.com>
2024-08-18 14:13:25 +0200

    Fix

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -2,18 +2,21 @@
 param()
 
 $params = @{
-    Name            = 'GitHub'
-    Repository      = 'PSGallery'
-    TrustRepository = $true
-    Prerelease      = $env:GITHUB_ACTION_INPUT_Prerelease -eq 'true'
+    Name = 'GitHub'
 }
 if (-not [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_Version)) {
     $params['Version'] = $env:GITHUB_ACTION_INPUT_Version
 }
 
-$alreadyInstalled = Get-InstalledPSResource -Name $params['Name'] -Version $params['Version']
+$installParams = @{
+    Repository      = 'PSGallery'
+    TrustRepository = $true
+    Prerelease      = $env:GITHUB_ACTION_INPUT_Prerelease -eq 'true'
+}
+
+$alreadyInstalled = Get-InstalledPSResource @params
 if (-not $alreadyInstalled) {
-    Install-PSResource @params
+    Install-PSResource @installParams
 }
 
 $alreadyImported = Get-Module -Name $params['Name'] -Refresh
