c2636d47a50401aaaf908d36af4f4cd62853f196
Marius Storhaug <marstor@hotmail.com>
2024-12-01 20:46:15 +0100

    Refactor Action-Test workflow to streamline output handling and remove unused scripts

--- scripts/outputs.ps1
+++
@@ -1,6 +0,0 @@
-﻿
-Get-Content -Path $env:GITHUB_OUTPUT
-# $outputs = Get-GitHubOutput -Path $env:GITHUB_OUTPUT
-# $outputs
-# $env:PSMODULE_GITHUB_SCRIPT = $false
-# Set-GitHubOutput -Name 'result' -Value $outputs.result
