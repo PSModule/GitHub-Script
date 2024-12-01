c4550e735dcb3cce69b57539262666cf2f0d4bd0
Marius Storhaug <marstor@hotmail.com>
2024-12-01 20:23:09 +0100

    Update outputs.ps1 to fetch outputs using GITHUB_OUTPUT path

--- scripts/outputs.ps1
+++ scripts/outputs.ps1
@@ -1,6 +1,6 @@
 ﻿
 Get-Content -Path $env:GITHUB_OUTPUT
-$outputs = Get-GitHubOutput
+$outputs = Get-GitHubOutput -Path $env:GITHUB_OUTPUT
 $outputs
 $env:PSMODULE_GITHUB_SCRIPT = $false
 Set-GitHubOutput -Name 'result' -Value $outputs.result
