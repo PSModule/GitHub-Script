988a654b26008d762473c100a64cffb435dd67a4
Marius Storhaug <marstor@hotmail.com>
2024-12-11 00:52:41 +0100

    cleanup

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -55,14 +55,9 @@ Write-Output 'GitHub module configuration:'
 Get-GitHubConfig | Select-Object Name, ID, RunEnv | Format-Table -AutoSize
 
 '::endgroup::'
-
 $providedToken = -not [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_Token)
 $providedClientID = -not [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_ClientID)
 $providedPrivateKey = -not [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_PrivateKey)
-Write-Verbose 'Provided authentication info:'
-Write-Verbose "Token:      [$providedToken]"
-Write-Verbose "ClientID:   [$providedClientID]"
-Write-Verbose "PrivateKey: [$providedPrivateKey]"
 
 if ($providedClientID -and $providedPrivateKey) {
     LogGroup 'Connecting using provided GitHub App' {
