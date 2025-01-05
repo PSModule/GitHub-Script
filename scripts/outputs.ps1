b47a03cef379edfaf3b1cccf9d272685c9da247d
Marius Storhaug <marstor@hotmail.com>
2025-01-05 20:55:10 +0100

    🩹 [Patch]: Optimize output retrieval and improve warning messages

--- scripts/outputs.ps1
+++ scripts/outputs.ps1
@@ -4,18 +4,23 @@ param()
 $DebugPreference = 'SilentlyContinue'
 $VerbosePreference = 'SilentlyContinue'
 
+$result = (Get-GitHubOutput).result
+if (-not $result) {
+    return
+}
+
 Write-Host '┏━━━━━┫ GitHub-Script ┣━━━━━┓'
 
 LogGroup ' - Outputs' {
     if ([string]::IsNullOrEmpty($env:GITHUB_ACTION)) {
-        Write-Warning 'Outputs cannot be accessed as the step has no ID.'
+        Write-GitHubWarning 'Outputs cannot be accessed as the step has no ID.'
     }
 
     if (-not (Test-Path -Path $env:GITHUB_OUTPUT)) {
         Write-Warning "File not found: $env:GITHUB_OUTPUT"
     }
 
-    (Get-GitHubOutput).result | Format-List
+    $result | Format-List
     Write-Host "Access outputs using `${{ fromJson(steps.$env:GITHUB_ACTION.outputs.result).<output-name> }}"
 }
 
