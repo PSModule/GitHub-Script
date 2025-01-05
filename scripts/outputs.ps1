081aac406b53f54e94e077f9cb49ce23432ed457
Marius Storhaug <marstor@hotmail.com>
2025-01-05 20:21:52 +0100

    Add log of outputs

---
+++ scripts/outputs.ps1
@@ -0,0 +1,22 @@
+﻿[CmdletBinding()]
+param()
+
+$DebugPreference = 'SilentlyContinue'
+$VerbosePreference = 'SilentlyContinue'
+
+Write-Host '┏━━━━━┫ GitHub-Script ┣━━━━━┓'
+
+LogGroup ' - Outputs' {
+    if ([string]::IsNullOrEmpty($env:GITHUB_ACTION)) {
+        Write-Warning 'Outputs cannot be accessed as the step has no ID.'
+    }
+
+    if (-not (Test-Path -Path $env:GITHUB_OUTPUT)) {
+        Write-Warning "File not found: $env:GITHUB_OUTPUT"
+    }
+
+    Get-GitHubOutput | Format-List
+    Write-Host "Access outputs using `${{ fromJson(steps.$env:GITHUB_ACTION.outputs.result).<output-name> }}"
+}
+
+Write-Host '┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛'
