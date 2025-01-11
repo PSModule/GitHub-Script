fe6f2a997a61221f21f0fb82b442158b71883eaa
Marius Storhaug <marstor@hotmail.com>
2025-01-11 10:43:48 +0100

    🩹 [Patch]: Enhance debug output and error handling in outputs.ps1 for better traceability

--- scripts/outputs.ps1
+++ scripts/outputs.ps1
@@ -2,37 +2,43 @@
 param()
 
 begin {
-    Write-Debug "[outputs] - Start"
+    Write-Debug '[outputs] - Start'
 }
 
 process {
-    $DebugPreference = 'SilentlyContinue'
-    $VerbosePreference = 'SilentlyContinue'
+    try {
+        $DebugPreference = 'SilentlyContinue'
+        $VerbosePreference = 'SilentlyContinue'
 
-    if ($env:GITHUB_ACTION_INPUT_ShowOutput -ne 'true') {
-        return
-    }
-
-    $result = (Get-GitHubOutput).result
-    if (-not $result) {
-        return
-    }
-    Write-Host '┏━━━━━┫ GitHub-Script ┣━━━━━┓'
-    LogGroup ' - Outputs' {
-        if ([string]::IsNullOrEmpty($env:GITHUB_ACTION)) {
-            Write-GitHubWarning 'Outputs cannot be accessed as the step has no ID.'
+        Write-Debug "[outputs] - ShowOutput: $env:GITHUB_ACTION_INPUT_ShowOutput"
+        if ($env:GITHUB_ACTION_INPUT_ShowOutput -ne 'true') {
+            return
         }
 
-        if (-not (Test-Path -Path $env:GITHUB_OUTPUT)) {
-            Write-Warning "File not found: $env:GITHUB_OUTPUT"
+        $result = (Get-GitHubOutput).result
+        Write-Debug "[outputs] - Result: $(-not $result)"
+        if (-not $result) {
+            return
         }
+        Write-Host '┏━━━━━┫ GitHub-Script ┣━━━━━┓'
+        LogGroup ' - Outputs' {
+            if ([string]::IsNullOrEmpty($env:GITHUB_ACTION)) {
+                Write-GitHubWarning 'Outputs cannot be accessed as the step has no ID.'
+            }
 
-        $result | Format-List
-        Write-Host "Access outputs using `${{ fromJson(steps.$env:GITHUB_ACTION.outputs.result).<output-name> }}"
+            if (-not (Test-Path -Path $env:GITHUB_OUTPUT)) {
+                Write-Warning "File not found: $env:GITHUB_OUTPUT"
+            }
+
+            $result | Format-List
+            Write-Host "Access outputs using `${{ fromJson(steps.$env:GITHUB_ACTION.outputs.result).<output-name> }}"
+        }
+        Write-Host '┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛'
+    } catch {
+        throw $_
     }
-    Write-Host '┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛'
 }
 
 end {
-    Write-Debug "[outputs] - End"
+    Write-Debug '[outputs] - End'
 }
