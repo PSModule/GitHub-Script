15b36d842fc6583b582ad76fd96ed9a393ae1d8f
Marius Storhaug <marstor@hotmail.com>
2025-01-11 21:57:09 +0100

    🩹 [Patch]: Add debug-logging (#22)
    ## Description
    
    This pull request includes several enhancements to the PowerShell
    scripts to improve logging, error handling, and overall structure. The
    most important changes are the addition of `begin`, `process`, and `end`
    blocks, improved error handling, and enhanced logging for better
    debugging.
    
    ### Enhancements to logging and structure:
    
    *
    [`scripts/main.ps1`](diffhunk://#diff-dc2e5a659836b1b73abb03421c567f5018c2755677c4a0aa764cb26117b68011R4-R9):
    Added `begin`, `process`, and `end` blocks to provide a clearer
    structure and added debug messages at the start and end of the script.
    [[1]](diffhunk://#diff-dc2e5a659836b1b73abb03421c567f5018c2755677c4a0aa764cb26117b68011R4-R9)
    [[2]](diffhunk://#diff-dc2e5a659836b1b73abb03421c567f5018c2755677c4a0aa764cb26117b68011L75-R108)
    *
    [`scripts/outputs.ps1`](diffhunk://#diff-ee715ca93229232e95883bf00629fd14e3bf174cdc17b723c4cc5d70e6a60a58R4-L17):
    Added `begin`, `process`, and `end` blocks to provide a clearer
    structure and added debug messages at the start and end of the script.
    [[1]](diffhunk://#diff-ee715ca93229232e95883bf00629fd14e3bf174cdc17b723c4cc5d70e6a60a58R4-L17)
    [[2]](diffhunk://#diff-ee715ca93229232e95883bf00629fd14e3bf174cdc17b723c4cc5d70e6a60a58L30-R43)
    
    ### Improvements to error handling:
    
    *
    [`scripts/main.ps1`](diffhunk://#diff-dc2e5a659836b1b73abb03421c567f5018c2755677c4a0aa764cb26117b68011L37-R46):
    Enhanced error handling by adding `-ErrorAction Stop` to the
    `Install-PSResource` command and logging warnings for exceptions.
    *
    [`scripts/main.ps1`](diffhunk://#diff-dc2e5a659836b1b73abb03421c567f5018c2755677c4a0aa764cb26117b68011L75-R108):
    Wrapped the main process block in a `try` block to catch and rethrow
    exceptions, ensuring any errors are properly handled and logged.
    *
    [`scripts/outputs.ps1`](diffhunk://#diff-ee715ca93229232e95883bf00629fd14e3bf174cdc17b723c4cc5d70e6a60a58L30-R43):
    Wrapped the main process block in a `try` block to catch and rethrow
    exceptions, ensuring any errors are properly handled and logged.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,93 +1,108 @@
 [CmdletBinding()]
 param()
 
-$env:PSMODULE_GITHUB_SCRIPT = $true
-Write-Host '┏━━━━━┫ GitHub-Script ┣━━━━━┓'
-Write-Host '::group:: - Setup GitHub PowerShell'
-
-$Name = 'GitHub'
-$Version = [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_Version) ? $null : $env:GITHUB_ACTION_INPUT_Version
-$Prerelease = $env:GITHUB_ACTION_INPUT_Prerelease -eq 'true'
-
-$alreadyInstalled = Get-InstalledPSResource -Name $Name -ErrorAction SilentlyContinue
-if ($Version) {
-    Write-Verbose "Filtering by version: $Version"
-    $alreadyInstalled = $alreadyInstalled | Where-Object Version -EQ $Version
+begin {
+    Write-Debug '[main] - Start'
 }
-if ($Prerelease) {
-    Write-Verbose 'Filtering by prerelease'
-    $alreadyInstalled = $alreadyInstalled | Where-Object Prerelease -EQ $Prerelease
-}
-Write-Verbose 'Already installed:'
-$alreadyInstalled | Format-Table
-if (-not $alreadyInstalled) {
-    $params = @{
-        Name            = $Name
-        Repository      = 'PSGallery'
-        TrustRepository = $true
-        Prerelease      = $Prerelease
-    }
-    if ($Version) {
-        $params['Version'] = $Version
-    }
-    $Count = 5
-    $Delay = 10
-    for ($i = 1; $i -le $Count; $i++) {
-        try {
-            Install-PSResource @params
-            break
-        } catch {
-            if ($i -eq $Count) {
-                throw $_
-            }
-            Start-Sleep -Seconds $Delay
+
+process {
+    try {
+        $env:PSMODULE_GITHUB_SCRIPT = $true
+        Write-Host '┏━━━━━┫ GitHub-Script ┣━━━━━┓'
+        Write-Host '::group:: - Setup GitHub PowerShell'
+
+        $Name = 'GitHub'
+        $Version = [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_Version) ? $null : $env:GITHUB_ACTION_INPUT_Version
+        $Prerelease = $env:GITHUB_ACTION_INPUT_Prerelease -eq 'true'
+
+        $alreadyInstalled = Get-InstalledPSResource -Name $Name -ErrorAction SilentlyContinue
+        if ($Version) {
+            Write-Verbose "Filtering by version: $Version"
+            $alreadyInstalled = $alreadyInstalled | Where-Object Version -EQ $Version
         }
+        if ($Prerelease) {
+            Write-Verbose 'Filtering by prerelease'
+            $alreadyInstalled = $alreadyInstalled | Where-Object Prerelease -EQ $Prerelease
+        }
+        Write-Verbose 'Already installed:'
+        $alreadyInstalled | Format-Table
+        if (-not $alreadyInstalled) {
+            $params = @{
+                Name            = $Name
+                Repository      = 'PSGallery'
+                TrustRepository = $true
+                Prerelease      = $Prerelease
+            }
+            if ($Version) {
+                $params['Version'] = $Version
+            }
+            $Count = 5
+            $Delay = 10
+            for ($i = 1; $i -le $Count; $i++) {
+                try {
+                    Install-PSResource @params -ErrorAction Stop
+                    break
+                } catch {
+                    Write-Warning $_.Exception.Message
+                    if ($i -eq $Count) {
+                        throw $_
+                    }
+                    Start-Sleep -Seconds $Delay
+                }
+            }
+        }
+
+        $alreadyImported = Get-Module -Name $Name
+        Write-Verbose 'Already imported:'
+        $alreadyImported | Format-Table
+        if (-not $alreadyImported) {
+            Write-Verbose "Importing module: $Name"
+            Import-Module -Name $Name
+        }
+
+        $providedToken = -not [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_Token)
+        $providedClientID = -not [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_ClientID)
+        $providedPrivateKey = -not [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_PrivateKey)
+        [pscustomobject]@{
+            Name                  = $Name
+            Version               = [string]::IsNullOrEmpty($Version) ? 'latest' : $Version
+            Prerelease            = $Prerelease
+            'Already installed'   = $null -ne $alreadyInstalled
+            'Already imported'    = $null -ne $alreadyImported
+            'Provided Token'      = $providedToken
+            'Provided ClientID'   = $providedClientID
+            'Provided PrivateKey' = $providedPrivateKey
+        } | Format-List
+        Write-Host '::endgroup::'
+
+        LogGroup ' - Installed modules' {
+            Get-InstalledPSResource | Select-Object Name, Version, Prerelease | Sort-Object -Property Name | Format-Table -AutoSize
+        }
+
+        if ($providedClientID -and $providedPrivateKey) {
+            LogGroup ' - GitHub connection - GitHub App' {
+                Connect-GitHub -ClientID $env:GITHUB_ACTION_INPUT_ClientID -PrivateKey $env:GITHUB_ACTION_INPUT_PrivateKey -Silent -PassThru |
+                    Format-List
+            }
+        } elseif ($providedToken) {
+            LogGroup ' - GitHub connection - Token' {
+                Connect-GitHub -Token $env:GITHUB_ACTION_INPUT_Token -Silent -PassThru |
+                    Format-List
+            }
+        }
+
+        LogGroup ' - Configuration' {
+            Get-GitHubConfig | Format-List
+        }
+
+        Write-Host '┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛'
+    } catch {
+        throw $_
     }
 }
 
-$alreadyImported = Get-Module -Name $Name
-Write-Verbose 'Already imported:'
-$alreadyImported | Format-Table
-if (-not $alreadyImported) {
-    Write-Verbose "Importing module: $Name"
-    Import-Module -Name $Name
+end {
+    Write-Debug '[main] - End'
+    $DebugPreference = $env:GITHUB_ACTION_INPUT_Debug -eq 'true' ? 'Continue' : 'SilentlyContinue'
+    $VerbosePreference = $env:GITHUB_ACTION_INPUT_Verbose -eq 'true' ? 'Continue' : 'SilentlyContinue'
 }
-
-$providedToken = -not [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_Token)
-$providedClientID = -not [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_ClientID)
-$providedPrivateKey = -not [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_PrivateKey)
-[pscustomobject]@{
-    Name                  = $Name
-    Version               = [string]::IsNullOrEmpty($Version) ? 'latest' : $Version
-    Prerelease            = $Prerelease
-    'Already installed'   = $null -ne $alreadyInstalled
-    'Already imported'    = $null -ne $alreadyImported
-    'Provided Token'      = $providedToken
-    'Provided ClientID'   = $providedClientID
-    'Provided PrivateKey' = $providedPrivateKey
-} | Format-List
-Write-Host '::endgroup::'
-
-LogGroup ' - Installed modules' {
-    Get-InstalledPSResource | Select-Object Name, Version, Prerelease | Sort-Object -Property Name | Format-Table -AutoSize
-}
-
-LogGroup ' - GitHub connection' {
-    if ($providedClientID -and $providedPrivateKey) {
-        Write-Verbose 'Connected using provided GitHub App'
-        Connect-GitHub -ClientID $env:GITHUB_ACTION_INPUT_ClientID -PrivateKey $env:GITHUB_ACTION_INPUT_PrivateKey -Silent
-    } elseif ($providedToken) {
-        Write-Verbose 'Connected using provided token'
-        Connect-GitHub -Token $env:GITHUB_ACTION_INPUT_Token -Silent
-    }
-    Get-GitHubContext | Format-List
-}
-
-LogGroup ' - Configuration' {
-    Get-GitHubConfig | Format-List
-}
-
-Write-Host '┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛'
-
-$DebugPreference = $env:GITHUB_ACTION_INPUT_Debug -eq 'true' ? 'Continue' : 'SilentlyContinue'
-$VerbosePreference = $env:GITHUB_ACTION_INPUT_Verbose -eq 'true' ? 'Continue' : 'SilentlyContinue'
--- scripts/outputs.ps1
+++ scripts/outputs.ps1
@@ -1,31 +1,43 @@
 ﻿[CmdletBinding()]
 param()
 
-$DebugPreference = 'SilentlyContinue'
-$VerbosePreference = 'SilentlyContinue'
-
-if ($env:GITHUB_ACTION_INPUT_ShowOutput -ne 'true') {
-    return
+begin {
+    $DebugPreference = 'SilentlyContinue'
+    $VerbosePreference = 'SilentlyContinue'
+    Write-Debug '[outputs] - Start'
 }
 
-$result = (Get-GitHubOutput).result
-if (-not $result) {
-    return
-}
+process {
+    try {
+        Write-Debug "[outputs] - ShowOutput: $env:GITHUB_ACTION_INPUT_ShowOutput"
+        if ($env:GITHUB_ACTION_INPUT_ShowOutput -ne 'true') {
+            return
+        }
 
-Write-Host '┏━━━━━┫ GitHub-Script ┣━━━━━┓'
+        $result = (Get-GitHubOutput).result
+        Write-Debug "[outputs] - Result: $(-not $result)"
+        if (-not $result) {
+            return
+        }
+        Write-Host '┏━━━━━┫ GitHub-Script ┣━━━━━┓'
+        LogGroup ' - Outputs' {
+            if ([string]::IsNullOrEmpty($env:GITHUB_ACTION)) {
+                Write-GitHubWarning 'Outputs cannot be accessed as the step has no ID.'
+            }
 
-LogGroup ' - Outputs' {
-    if ([string]::IsNullOrEmpty($env:GITHUB_ACTION)) {
-        Write-GitHubWarning 'Outputs cannot be accessed as the step has no ID.'
+            if (-not (Test-Path -Path $env:GITHUB_OUTPUT)) {
+                Write-Warning "File not found: $env:GITHUB_OUTPUT"
+            }
+
+            $result | Format-List
+            Write-Host "Access outputs using `${{ fromJson(steps.$env:GITHUB_ACTION.outputs.result).<output-name> }}"
+        }
+        Write-Host '┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛'
+    } catch {
+        throw $_
     }
-
-    if (-not (Test-Path -Path $env:GITHUB_OUTPUT)) {
-        Write-Warning "File not found: $env:GITHUB_OUTPUT"
-    }
-
-    $result | Format-List
-    Write-Host "Access outputs using `${{ fromJson(steps.$env:GITHUB_ACTION.outputs.result).<output-name> }}"
 }
 
-Write-Host '┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛'
+end {
+    Write-Debug '[outputs] - End'
+}
