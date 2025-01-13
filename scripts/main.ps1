a07d8a035384f977a6ef4efdbd2c250523ac8bb2
Marius Storhaug <marstor@hotmail.com>
2025-01-13 17:22:03 +0100

    🩹 [Patch]: Enhance Git configuration logging to include system, global, and local scopes

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -95,7 +95,15 @@ process {
             Get-GitHubConfig | Format-List
         }
 
-        LogGroup ' - Git Configuration' {
+        LogGroup ' - Git Configuration - System' {
+            Get-GitHubGitConfig -Scope system | Format-List
+        }
+
+        LogGroup ' - Git Configuration - Global' {
+            Get-GitHubGitConfig -Scope global | Format-List
+        }
+
+        LogGroup ' - Git Configuration - Local' {
             Get-GitHubGitConfig | Format-List
         }
 
